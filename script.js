f3ef81928c2bed8067590cb579fb32885eb9ec72
Cachi <ezequielcachi@gmail.com>
2024-10-21 17:26:26 -0300

    continuing with errors

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ const termError = document.getElementById("term-error");
 const percentageError = document.getElementById("percentage-error");
 const radioError = document.getElementById("radio-error");
 
+//------------ BUTTONS -------------------//
+
+const poundButton = document.querySelector(".pound-button");
+const yearsButton = document.querySelector(".years-button");
+const percentageButton = document.querySelector(".percentage-buttons");
+
+//------------ INPUT CONTAINERS -------------------//
+
+inputContainer = document.querySelector(".input-container");
+
 inputList.forEach((input) => {
   input.addEventListener("blur", (e) => {
     if (input.id === "repayment" || input.id === "interestOnly") return;
@@ -15,6 +25,9 @@ inputList.forEach((input) => {
       switch (input.id) {
         case "pounds":
           mortgageError.classList.remove("hidden");
+          poundButton.style.background = "hsl(4, 69%, 50%)";
+          poundButton.style.color = "#fff";
+          inputContainer.style.border = "1px solid hsl(4, 69%, 50%)";
           break;
         case "years":
           termError.classList.remove("hidden");
